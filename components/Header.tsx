4008549f8549137a089e29938097f55112143f92
Vicent Gozalbes <vigosan@gmail.com>
2024-04-02 21:03:48 +0200

    change close button position

--- components/Header.tsx
+++ components/Header.tsx
@@ -67,7 +67,7 @@ export function Header() {
       >
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white py-6 px-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-end">
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
