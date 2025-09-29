e0650b33af2dec6ab621b351976764ae46639bf2
Vicent Gozalbes <vigosan@gmail.com>
2025-09-29 19:29:40 +0200

    Show six featured pictures in carousel instead of three

--- pages/index.tsx
+++ pages/index.tsx
@@ -364,7 +364,7 @@ export default function IndexPage({ featuredPictures }: IndexPageProps) {
         {/* Carousel-style featured works */}
         <div className="relative">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-4">
-            {featuredPictures.slice(0, 3).map((picture, index) => (
+            {featuredPictures.slice(0, 6).map((picture) => (
               <div key={picture.id} className="group">
                 <Link href={`/pictures/${picture.slug}`}>
                   <div className="relative">
